f39920a9bf2ccc25241eb6c783eef16399a1514e
Rohan Vashisht <rohanprogrammer1@gmail.com>
2026-03-07 21:41:28 +0530

    Just got a DiGraph with 1011 nodes and 1206 edges. This got printed.

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import sqlite3
+import networkx
 
 
 def main():
     connection = sqlite3.connect("zigistry.db")
-    
+    cursor = connection.cursor()
+    cursor.execute(
+        "SELECT repo_id, dependent FROM repo_dependents"
+    )
+
+    repo2repo_connection = cursor.fetchall()
+
+    graph_builder = networkx.DiGraph()
+
+    for repo_1, repo_2 in repo2repo_connection:
+        graph_builder.add_edge(repo_2, repo_1)
+
+    print(graph_builder)
 
 if __name__ == "__main__":
     main()
